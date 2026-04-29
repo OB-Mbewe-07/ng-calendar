54504ae1120dfe9868d669b13e8d502b17e22148
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-04-29 09:17:42 +0200

    feat: month toggle working

--- src/app/components/upcoming-holiday.component.ts/upcoming-holiday.component.ts
+++ src/app/components/upcoming-holiday.component.ts/upcoming-holiday.component.ts
@@ -54,7 +54,7 @@ export class DataviewBasicDemo implements OnInit, OnDestroy {
     const yearStr = year.toString();
 
     this.filteredHolidays = this.holidays.filter((day) =>
-      day.date.startsWith(`${yearStr}-${monthStr}`)
+      day.date.startsWith(`${yearStr}-${monthStr}`),
     );
 
     const totalDays = new Date(year, month + 1, 0).getDate();
@@ -68,6 +68,21 @@ export class DataviewBasicDemo implements OnInit, OnDestroy {
     const dayStr = day.toString().padStart(2, '0');
     const fullDate = `${this.currentDate.getFullYear() - 1}-${(this.currentDate.getMonth() + 1).toString().padStart(2, '0')}-${dayStr}`;
     return this.filteredHolidays.find((holiday) => holiday.date === fullDate);
+  }
+
+  prevMonth() {
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
+    this.generateCalendar();
+  }
+
+  nextMonth() {
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 1);
+    this.generateCalendar();
+  }
+
+  goToToday() {
+    this.currentDate = new Date();
+    this.generateCalendar();
   }
 
   ngOnDestroy(): void {
