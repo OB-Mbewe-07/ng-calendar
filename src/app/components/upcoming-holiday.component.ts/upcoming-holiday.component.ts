e6231d8d824df0dd4c14e9371a5436fbf5f0575e
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-04-29 09:05:17 +0200

    feat: working calendar display

--- src/app/components/upcoming-holiday.component.ts/upcoming-holiday.component.ts
+++ src/app/components/upcoming-holiday.component.ts/upcoming-holiday.component.ts
@@ -7,7 +7,7 @@ import { Holiday } from '../../shared/models/data.models';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { BadgeModule } from 'primeng/badge';
-import { AddTaskComponent } from "../add-task.component/add-task.component";
+import { AddTaskComponent } from '../add-task.component/add-task.component';
 
 @Component({
   selector: 'app-upcoming',
@@ -27,14 +27,16 @@ export class DataviewBasicDemo implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.allHolidays();
-    this.generateCalendar();
   }
+
+  filteredHolidays: Holiday[] = [];
 
   allHolidays() {
     this.subscription.add(
       this.holidayApi.getHolidays('US', 2025).subscribe({
         next: (data) => {
           this.holidays = data.holidays;
+          this.generateCalendar();
           this.cdr.detectChanges();
         },
         error: (err) => {
@@ -45,14 +47,27 @@ export class DataviewBasicDemo implements OnInit, OnDestroy {
   }
 
   generateCalendar() {
-    const year = this.currentDate.getFullYear();
+    const year = this.currentDate.getFullYear() - 1; //2025
     const month = this.currentDate.getMonth();
+
+    const monthStr = (month + 1).toString().padStart(2, '0');
+    const yearStr = year.toString();
+
+    this.filteredHolidays = this.holidays.filter((day) =>
+      day.date.startsWith(`${yearStr}-${monthStr}`)
+    );
 
     const totalDays = new Date(year, month + 1, 0).getDate();
     this.daysInMonth = Array.from({ length: totalDays }, (_, i) => i + 1);
 
     const firstDayIndex = new Date(year, month, 1).getDay();
     this.gridPadding = Array.from({ length: firstDayIndex }, (_, i) => i);
+  }
+
+  getHolidayForDay(day: number) {
+    const dayStr = day.toString().padStart(2, '0');
+    const fullDate = `${this.currentDate.getFullYear() - 1}-${(this.currentDate.getMonth() + 1).toString().padStart(2, '0')}-${dayStr}`;
+    return this.filteredHolidays.find((holiday) => holiday.date === fullDate);
   }
 
   ngOnDestroy(): void {
